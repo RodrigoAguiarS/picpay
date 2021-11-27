a8f8dbd3e4b3cdf5429f13735a23f55ad5b35916
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-11-27 00:49:15 -0300

    Initial commit

---
+++ src/main/java/br/com/rodrigo/picpay/model/EntidadeBase.java
@@ -0,0 +1,12 @@
+package br.com.rodrigo.picpay.model;
+
+public class EntidadeBase {
+    private long id;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+}
---
+++ src/main/java/br/com/rodrigo/picpay/model/Usuario.java
@@ -0,0 +1,4 @@
+package br.com.rodrigo.picpay.model;
+
+public class Usuario extends EntidadeBase{
+}
